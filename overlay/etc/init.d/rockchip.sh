TinkerBoard/rockchip-linux-debian

leds: Support led control for emmc or sdcard read/write
Change-Id: Ife1c52ae3c89b8c190924913c68e5f43838e9653
Signed-off-by: Rock Shen <rock_shen@asus.com>

Former-commit-id: 952bfb2084539c66be111b6750bae8b4a531065e

overlay/etc/init.d/rockchip.sh
@@ -140,6 +140,18 @@ service async start
 # enable adbd service
 service adbd start
 
+# set act-led trigger function
+cmdline=$(cat /proc/cmdline)
+storage=`echo $cmdline|awk '{print match($0,"storagemedia=emmc")}'`;
+
+if [ $storage -gt 0 ]; then
+    #emmc
+    echo mmc2 > /sys/class/leds/act-led/trigger
+else
+    #sdcard
+    echo mmc1 > /sys/class/leds/act-led/trigger
+fi
+
 # set cpu governor and frequence
 CPU_GOVERNOR=$(cat /boot/config.txt | grep 'cpu_governor' | cut -d '=' -f2)
 A53_MINFREQ=$(cat /boot/config.txt | grep 'a53_minfreq' | cut -d '=' -f2)

rageworx/rk3399-debian11

Change-Id: I0cb4342c38b6a440e16533e55d61869fc81f18c2

overlay/etc/init.d/rockchip.sh
@@ -135,6 +135,18 @@ fi
 # enable adbd service
 #service adbd start
 
+# set act-led trigger function
+cmdline=$(cat /proc/cmdline)
+storage=`echo $cmdline|awk '{print match($0,"storagemedia=emmc")}'`;
+
+if [ $storage -gt 0 ]; then
+    #emmc
+    echo mmc0 > /sys/class/leds/act-led/trigger
+else
+    #sdcard
+    echo mmc1 > /sys/class/leds/act-led/trigger
+fi
+
 # set cpu governor and frequence
 CPU_GOVERNOR=$(cat /boot/config.txt | grep 'cpu_governor' | cut -d '=' -f2)
 A53_MINFREQ=$(cat /boot/config.txt | grep 'a53_minfreq' | cut -d '=' -f2)