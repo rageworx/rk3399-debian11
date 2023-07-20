e63480ba7f08838fa193517fd39e0e6ba876bc9d
Rock Shen <rock_shen@asus.com>
2023-07-20 14:30:20 +0800

    Test_tool: clear rtc alarm before set rtc alarm
    Fix set rtc alarm failed.
    /etc/rc_shutdown.sh: line 11: echo: write error: Device or resource busy
    
    Change-Id: I6ccffc9c33ad1478d27517a62c8277097d821a18
    Signed-off-by: Rock Shen <rock_shen@asus.com>

--- overlay-debug/home/linaro/Desktop/Test_tool/System_test/etc/rc_shutdown.sh
+++ overlay-debug/home/linaro/Desktop/Test_tool/System_test/etc/rc_shutdown.sh
@@ -8,6 +8,7 @@ echo "########## Shutdown device from test_shutdown.sh ##########"
 echo "########## shutdown_times = $times  ##########"
 ((times+=1))
 echo "shutdown_times = "$times > /etc/shutdown_times.txt
+echo 0 > /sys/class/rtc/rtc0/wakealarm
 echo +40 > /sys/class/rtc/rtc0/wakealarm
 #systemctl poweroff
 poweroff -f
