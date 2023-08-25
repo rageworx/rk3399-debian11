7b0f30ac6a162e9bc63b5dedc8c770c6fcb3fe60
Jeffy Chen <jeffy.chen@rock-chips.com>
2023-08-25 16:58:34 +0800

    mk-image.sh: Speed up image making
    Change-Id: I564346bcfb3d17ad0bb17871e531b58f93fe5436
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>
    
    Former-commit-id: 6751ec0d3948bb9e59c6f750978d029996c16f40

--- mk-image.sh
+++ mk-image.sh
@@ -17,8 +17,6 @@ for script in ./post-build.sh ../device/rockchip/common/post-build.sh; do
 	sudo $script "$(realpath "$TARGET_ROOTFS_DIR")"
 done
 
-dd if=/dev/zero of=${ROOTFSIMAGE} bs=1M count=0 seek=${IMAGE_SIZE_MB}
-
-sudo mkfs.ext4 -d ${TARGET_ROOTFS_DIR} ${ROOTFSIMAGE}
+sudo mkfs.ext4 -d ${TARGET_ROOTFS_DIR} ${ROOTFSIMAGE} ${IMAGE_SIZE_MB}M
 
 echo Rootfs Image: ${ROOTFSIMAGE}
