d0a55ddb90733fbc450fe4d6c6bdf4645a908e3b
Caesar Wang <wxt@rock-chips.com>
2023-08-30 18:10:12 +0800

    overlay: update rkwifbt.sh
    In order to get the tty dev number with kernel devicetree.
    
    Change-Id: I685fb3dfcce1b53b9dfdfdedcd341675c2c4aebb
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>
    
    Former-commit-id: fe99b13e2e2111e1f4afdd5b130e6c657ee0de3b

--- overlay/etc/init.d/rkwifibt.sh
+++ overlay/etc/init.d/rkwifibt.sh
@@ -14,31 +14,31 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 init_rkwifibt() {
     case $1 in
         rk3288)
-	    rk_wifibt_init /dev/ttyS0
+	    rk_wifibt_init /dev/$(bt-tty)
             ;;
         rk3399|rk3399pro)
-	    rk_wifibt_init /dev/ttyS0
+	    rk_wifibt_init /dev/$(bt-tty)
             ;;
         rk3328)
-	    rk_wifibt_init /dev/ttyS0
+	    rk_wifibt_init /dev/$(bt-tty)
             ;;
         rk3326|px30)
-	    rk_wifibt_init /dev/ttyS1
+	    rk_wifibt_init /dev/$(bt-tty)
             ;;
         rk3128|rk3036)
-	    rk_wifibt_init /dev/ttyS0
+	    rk_wifibt_init /dev/$(bt-tty)
             ;;
         rk3562)
-	    rk_wifibt_init /dev/ttyS1
+	    rk_wifibt_init /dev/$(bt-tty)
             ;;
         rk3566)
-	    rk_wifibt_init /dev/ttyS1
+	    rk_wifibt_init /dev/$(bt-tty)
             ;;
         rk3568)
-	    rk_wifibt_init /dev/ttyS8
+	    rk_wifibt_init /dev/$(bt-tty)
             ;;
         rk3588|rk3588s)
-	    rk_wifibt_init /dev/ttyS8
+	    rk_wifibt_init /dev/$(bt-tty)
             ;;
     esac
 }
